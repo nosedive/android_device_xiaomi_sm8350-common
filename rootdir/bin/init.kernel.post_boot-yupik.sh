a6d034869ae08addfac0818d109d04a9bca5818d
Murat Kozan <muratkozan350@gmail.com>
2023-08-19 18:59:18 +0300

    rootdir: prefer deep over s2idle

--- rootdir/bin/init.kernel.post_boot-yupik.sh
+++ rootdir/bin/init.kernel.post_boot-yupik.sh
@@ -347,8 +347,8 @@ do
 	done
 done
 
-# set s2idle as default suspend mode
-echo s2idle > /sys/power/mem_sleep
+# set deep as default suspend mode
+echo deep > /sys/power/mem_sleep
 
 configure_memory_parameters
 
