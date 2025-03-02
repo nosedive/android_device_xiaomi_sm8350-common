e779b982eb22c3235cb91a3883979f8b6a7ec01d
nosedive <daniel@kreinig.com>
2025-03-02 18:28:28 +0100

    ndED: enable lz4 compression for zram

--- rootdir/bin/init.kernel.post_boot-yupik.sh
+++ rootdir/bin/init.kernel.post_boot-yupik.sh
@@ -53,9 +53,9 @@ function configure_zram_parameters() {
 		let zRamSizeMB=4096
 	fi
 
-	if [ "$low_ram" == "true" ]; then
+#	if [ "$low_ram" == "true" ]; then
 		echo lz4 > /sys/block/zram0/comp_algorithm
-	fi
+#	fi
 
 	if [ -f /sys/block/zram0/disksize ]; then
 		if [ -f /sys/block/zram0/use_dedup ]; then
